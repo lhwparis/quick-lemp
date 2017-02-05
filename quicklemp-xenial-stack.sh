5030e011863b6723a98f344c5d7f08684aefafd8
Timo <paris@lan-force.de>
2017-02-05 23:07:30 +0100

    Update quicklemp-xenial-stack.sh

--- quicklemp-xenial-stack.sh
+++ quicklemp-xenial-stack.sh
@@ -18,9 +18,10 @@ fi
 
 # Update packages and add MariaDB repository
 echo -e '\n[Package Updates]'
-apt-get install software-properties-common
-apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-add-apt-repository 'deb http://mirrors.syringanetworks.net/mariadb/repo/10.1/ubuntu xenial main'
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.hosteurope.de/mirror/mariadb.org/repo/10.1/ubuntu xenial main'
+
 add-apt-repository ppa:nginx/stable
 apt-get install -y language-pack-en-base
   LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
@@ -40,70 +41,70 @@ echo -e '\n[Nginx]'
 # remove apache2
 apt-get -y install nginx
 service nginx stop
-mv /etc/nginx /etc/nginx-previous
-curl -L https://github.com/h5bp/server-configs-nginx/archive/1.0.0.tar.gz | tar -xz
+#mv /etc/nginx /etc/nginx-previous
+#curl -L https://github.com/h5bp/server-configs-nginx/archive/1.0.0.tar.gz | tar -xz
 # Newer: https://github.com/h5bp/server-configs-nginx/archive/master.zip
-mv server-configs-nginx-1.0.0 /etc/nginx
-cp /etc/nginx-previous/fastcgi_params /etc/nginx
-sed -i.bak -e
-sed -i.bak -e "s/www www/www-data www-data/" \
-  -e "s/logs\/error.log/\/var\/log\/nginx\/error.log/" \
-  -e "s/logs\/access.log/\/var\/log\/nginx\/access.log/" /etc/nginx/nginx.conf
-sed -i.bak -e "s/logs\/static.log/\/var\/log\/nginx\/static.log/" /etc/nginx/h5bp/location/expires.conf
+#mv server-configs-nginx-1.0.0 /etc/nginx
+#cp /etc/nginx-previous/fastcgi_params /etc/nginx
+#sed -i.bak -e
+#sed -i.bak -e "s/www www/www-data www-data/" \
+#  -e "s/logs\/error.log/\/var\/log\/nginx\/error.log/" \
+#  -e "s/logs\/access.log/\/var\/log\/nginx\/access.log/" /etc/nginx/nginx.conf
+#sed -i.bak -e "s/logs\/static.log/\/var\/log\/nginx\/static.log/" /etc/nginx/h5bp/location/expires.conf
 
-echo
-read -p 'Do you want to create a self-signed SSL cert and configure HTTPS? [y/N] ' -n 1 -r
-echo
+#echo
+#read -p 'Do you want to create a self-signed SSL cert and configure HTTPS? [y/N] ' -n 1 -r
+#echo
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  conf1="  listen 443 ssl default_server;\n"
-  conf2="  include h5bp/directive-only/ssl.conf;\n  ssl_certificate /etc/ssl/certs/nginx.crt;\n  ssl_certificate_key /etc/ssl/private/nginx.key;"
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt
-  chmod 400 /etc/ssl/private/nginx.key
-else
-  conf1=
-  conf2=
-  conf3=
-fi
+#if [[ $REPLY =~ ^[Yy]$ ]]; then
+#  conf1="  listen 443 ssl default_server;\n"
+#  conf2="  include h5bp/directive-only/ssl.conf;\n  ssl_certificate /etc/ssl/certs/nginx.crt;\n  ssl_certificate_key /etc/ssl/private/nginx.key;"
+#  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt
+#  chmod 400 /etc/ssl/private/nginx.key
+#else
+#  conf1=
+#  conf2=
+#  conf3=
+#fi
+#
+#echo -e "server {
+#  listen 80 default_server;
+#$conf1
+#  server_name _;
+#
+#$conf2
+#  root /var/www/vhosts/default/public;
+#
+#  charset utf-8;
+#  error_page 404 /404.html;
+#  location = /favicon.ico { log_not_found off; access_log off; }
+#  location = /robots.txt { allow all; log_not_found off; access_log off; }
+#  client_max_body_size 20M;
+#
+#  location ^~ /static/ {
+#    alias /var/www/vhosts/default/static;
+#  }
+#
+#  location ~ \\.php\$ {
+#    try_files \$uri =404;
+#    fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+#    fastcgi_param SCRIPT_FILENAME \$request_filename;
+#    fastcgi_index index.php;
+#    include fastcgi_params;
+#  }
+#
+#}" > /etc/nginx/sites-available/default
 
-echo -e "server {
-  listen 80 default_server;
-$conf1
-  server_name _;
-
-$conf2
-  root /var/www/vhosts/default/public;
-
-  charset utf-8;
-  error_page 404 /404.html;
-  location = /favicon.ico { log_not_found off; access_log off; }
-  location = /robots.txt { allow all; log_not_found off; access_log off; }
-  client_max_body_size 20M;
-
-  location ^~ /static/ {
-    alias /var/www/vhosts/default/static;
-  }
-
-  location ~ \\.php\$ {
-    try_files \$uri =404;
-    fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
-    fastcgi_param SCRIPT_FILENAME \$request_filename;
-    fastcgi_index index.php;
-    include fastcgi_params;
-  }
-
-}" > /etc/nginx/sites-available/default
-
-mkdir -p /var/www/vhosts/default/public
-mkdir -p /var/www/vhosts/default/static
-ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+#mkdir -p /var/www/vhosts/default/public
+#mkdir -p /var/www/vhosts/default/static
+#ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 mariaDbPHP7=
 mariaDbPHP5=
 phpVersionInstalled=
 
 # PHP 7.x
-apt-get -q -y install php7.0-fpm php7.0-common php7.0-curl php7.0-gd php7.0-cli php-pear php7.0-imap php7.0-mcrypt php7.0-opcache php7.0-json php7.0-mbstring
+apt-get -q -y install php7.0-fpm php7.0-common php7.0-curl php7.0-gd php7.0-cli php-pear php7.0-imap php7.0-mcrypt php7.0-opcache php7.0-json php7.0-mbstring php7.0-dom php7.0-zip
 echo '<?php phpinfo(); ?>' > /var/www/vhosts/default/public/checkinfo.php
 
 # PHP 5.6.x
@@ -111,7 +112,7 @@ echo
 read -p 'Do you want to install PHP5.6 in addition to PHP7.0 (not working on ubuntu 16.04) [y/N] ' -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-apt-get -q -y install php5.6-fpm php5.6-common php5.6-curl php5.6-gd php5.6-cli php-pear php5.6-imap php5.6-mcrypt php5.6-opcache php5.6-json
+apt-get -q -y install php5.6-fpm php5.6-common php5.6-curl php5.6-gd php5.6-cli php-pear php5.6-imap php5.6-mcrypt php5.6-opcache php5.6-json php5.6-mbstring php5.6-dom php5.6-zip
 mariaDbPHP5= " php5.6-mysql"
 phpVersionInstalled= "php5"
 echo '<?php phpinfo(); ?>' > /var/www/vhosts/default/public/checkinfo.php
